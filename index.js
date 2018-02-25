feb718f1b4654d73ad596a8af5af487701920881
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2018-02-25 13:11:11 +0200

    Learning async/await with tests

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-const mongoUrl = process.env.MONGODB_URI
 mongoose
-  .connect(mongoUrl)
+  .connect(config.mongoUrl)
   .then(() => {
-    console.log('Connected to database', mongoUrl)
+    console.log('Connected to database', config.mongoUrl)
   })
   .catch(error => {
     console.log(error)
@@ -25,7 +21,16 @@ app.use(bodyParser.json())
 app.use(express.static('build'))
 app.use('/api/blogs', blogsRouter)
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
---
+++ tests/blog_api.test.js
@@ -0,0 +1,68 @@
+const supertest = require('supertest')
+const { app, server } = require('../index')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+beforeAll(async () => {
+  await Blog.remove({})
+  console.log('Cleared blogs')
+
+  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+  console.log('done')
+})
+
+afterAll(() => {
+  server.close()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Testiblogi',
+    author: 'John Doe',
+    url: '<url>',
+    likes: 1234
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api
+    .get('/api/blogs')
+
+  const contents = response.body.map(r => r.title)
+
+  expect(response.body.length).toBe(initialBlogs.length + 1)
+  expect(contents).toContain('Testiblogi')
+
+})
